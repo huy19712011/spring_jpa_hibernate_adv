7488e3d4600d9d488cf261f7bd0ac6e566e5bc31
huynq <huy19712011@gmail.com>
2023-01-07 19:25:37 +0700

    Initial commit

---
+++ src/main/java/com/example/spring_jpa_hibernate_adv/SpringJpaHibernateAdvApplication.java
@@ -0,0 +1,14 @@
+package com.example.spring_jpa_hibernate_adv;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringJpaHibernateAdvApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringJpaHibernateAdvApplication.class, args);
+        System.out.println("Ok...");
+    }
+
+}
---
+++ src/test/java/com/example/spring_jpa_hibernate_adv/SpringJpaHibernateAdvApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.spring_jpa_hibernate_adv;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringJpaHibernateAdvApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
