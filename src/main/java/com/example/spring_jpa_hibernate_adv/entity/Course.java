aeb50e85fa5fb9fbbbb6c9170935ee90dceeb479
huynq <huy19712011@gmail.com>
2023-01-07 19:40:25 +0700

    Step 1, 2: creating JPA Entity Course

---
+++ src/main/java/com/example/spring_jpa_hibernate_adv/entity/Course.java
@@ -0,0 +1,34 @@
+package com.example.spring_jpa_hibernate_adv.entity;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.Id;
+import org.slf4j.LoggerFactory;
+
+@Entity
+public class Course {
+
+    @Id
+    @GeneratedValue
+    private Long id;
+
+    private String name;
+
+    public Course(String name) {
+        this.name = name;
+    }
+
+    protected Course() {}
+
+    public Long getId() {
+        return id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+}
