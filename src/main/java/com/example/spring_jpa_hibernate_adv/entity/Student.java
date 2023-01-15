b50534afaaa8c567eeb672b3a3457ca0b8cf119a
huynq <huy19712011@gmail.com>
2023-01-15 17:50:47 +0700

    Step 37: ManyToMany mapping - customizing join table

--- src/main/java/com/example/spring_jpa_hibernate_adv/entity/Student.java
+++ src/main/java/com/example/spring_jpa_hibernate_adv/entity/Student.java
@@ -20,6 +20,12 @@ public class Student {
     private Passport passport;
 
     @ManyToMany
+    //following to create join table
+    // joinColumn - student_id
+    // inverseColumn - course_id
+    @JoinTable(name = "student_course",
+            joinColumns = @JoinColumn(name = "student_id"),
+            inverseJoinColumns = @JoinColumn(name = "course_id"))
     private List<Course> courses = new ArrayList<>();
 
     public Student(String name) {
